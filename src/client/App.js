f594fbc267cd40c880f262cb527d7cff4a65d469
johnnyliu <mayaisbad@gmail.com>
2020-10-24 13:31:22 -0400

    Test Oct 23

--- src/client/App.js
+++ src/client/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './app.css';
 import ReactImage from './react.png';
 import SimpleReactFileUpload from "./react-file-upload";
+
 export default class App extends Component {
   state = { username: null };
 
