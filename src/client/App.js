67cffe772aa51caac247849940d5ab2532ad16e9
johnnyliu <mayaisbad@gmail.com>
2021-02-16 18:31:35 -0500

    Implemented react page refresh for results

--- src/client/App.js
+++ src/client/App.js
@@ -1,45 +1,62 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './app.css';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+      done: false,
+      data: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    console.log(event.target.files[0]);
+    this.setState({ value: event.target.files[0] });
+  }
+
+  handleSubmit(event) {
+    /* add API call here */
+    const formData = new FormData();
+    formData.append('target_file', this.state.value);
+
+    axios
+      .post('/api/upload', formData)
+      .then((res) => {
+        this.setState({ done: true, data: res.data })
+        console.log(res);
+      })
+      .catch((err) => alert("File Upload Error"));
+    event.preventDefault();
+  }
 
   render() {
-    return (
-      <div className="App">
-        <h1 className="App-header">
-          <br/>
-             BIEN470 project: Nucleotide-counting placeholder algorithm
-          <br/>
-          <br/>
-          <br/>
-          <br/>
+    if (this.state.done) {
+      return (
+        <div className="App"> Success: {this.state.data} </div>
+      )
+    }
+    else {
+      return (
+        <div className="App">
+          <h1 className="App-header">
+            BIEN470 project: Nucleotide-counting placeholder algorithm
+          </h1>
 
-          <form method="POST" action="/api/upload" encType="multipart/form-data">
-            <input type="file" name="target_file" />
-            <br/>
-            <br/>
-            <br/>
-            <input type="submit" value="Parse nucleotides" />
+          <form method="POST" encType="multipart/form-data" onSubmit={this.handleSubmit} >
+            <input type="file" name="target_file" onChange={this.handleChange} />
+
+            <input type="submit" value="Parse nucleotides"/>
           </form>
-
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-
-        </h1>
-      </div>
+        </div>
 
 
-    );
+      );
+    }
   }
-}
+};
--- src/server/index.js
+++ src/server/index.js
@@ -7,8 +7,10 @@ const app = express();
 app.use(fileUpload({}));
 
 app.post('/api/upload', (req, res) => {
+  console.log("Started");
 
   if (!req.files || Object.keys(req.files).length === 0) {
+    console.log('no files')
     return res.status(400).send('No files were uploaded.');
   }
 
@@ -36,6 +38,7 @@ app.post('/api/upload', (req, res) => {
       fullString: str,
       'num': num
     };
+
     // respond with the object
     // If we want to send the old page, can just send toReturn instead!
     dataToSend = data;
@@ -49,34 +52,5 @@ app.post('/api/upload', (req, res) => {
   });
 
 });
-
-/*
-app.get('/run', (req, res) => {
-  let dataToSend;
-  const seq = '/Users/johnnyliu/Documents/GitHub/BIEN_470_interference/src/server/uploads/practicesequence.txt'; // instead of this, it would get the file from the request
-
-  // spawn new child process to call the python script
-  const python = spawn('python', ['./scripts/script1.py', seq]); // make sure script1.py is in same folder
-  // collect data from script
-  python.stdout.on('data', (data) => {
-    console.log('Pipe data from python script ...');
-    const str = data.toString();
-    // mock code to "parse" the response
-    const num = str.substr(13,1);
-    const toReturn = {
-      fullString: str,
-      'num': num
-    };
-    // respond with the object
-    dataToSend = toReturn;
-  });
-  // in close event we are sure that stream from child process is closed
-  python.on('close', (code) => {
-    console.log(`child process close all stdio with code ${code}`);
-    // send data to browser
-    res.send(dataToSend);
-  });
-});
- */
 
 app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
